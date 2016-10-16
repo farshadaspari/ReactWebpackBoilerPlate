ab81f812405732f67e2fa49cb840428e8f96f7d2
Farshad <farshadaspari@gmail.com>
2016-10-16 13:50:15 -0700

    more node modules and Express server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+﻿var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static('./dist'));
+
+app.use('/', function (req, res) {
+    res.sendFile(path.resolve('client/index.html'));
+});
+
+var port = 3000;
+
+app.listen(port, function (error) {
+    if (error) throw error;
+    console.log(' --> App is running at: http://localhost:3000');
+});
