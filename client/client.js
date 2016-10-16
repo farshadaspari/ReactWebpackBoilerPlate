44e775bc5429ed4c9c24b09e081f1137a5be1147
Farshad <farshadaspari@gmail.com>
2016-10-16 15:11:06 -0700

    Added the first react component 'App' and configured webpack to translate/transpile using es2015 and babel presets and budle everything into bundle.js available for server to serve!

---
+++ client/client.js
@@ -0,0 +1,5 @@
+﻿import React from 'react'
+import { render } from 'react-dom'
+import App from '../components/App.js'
+
+render(<App/>, document.getElementById('app'));
---
+++ components/App.js
@@ -0,0 +1,9 @@
+﻿import React, { Component } from 'react'
+
+class App extends Component{
+    render(){
+        return <div>Farshad's React Boilerplate!</div>
+    }
+}
+
+export default App
